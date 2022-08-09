1ee0d3076a57194d76e243d4eb3772f40f6d473e
Ben Kooijman <ben@kooijman.se>
2022-08-09 16:07:52 +0200

    Add a refresh button

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 # TODO:
-# - Create a refresh button
-# - Error handling for the following
-# {'bozo': True, 'entries': [], 'feed': {}, 'headers': {}, 'bozo_exception': URLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:997)'))}
-# {'bozo': True, 'entries': [], 'feed': {}, 'headers': {}, 'bozo_exception': URLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: self signed certificate in certificate chain (_ssl.c:997)'))}
-# {'bozo': True, 'entries': [], 'feed': {}, 'headers': {}, 'bozo_exception': URLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:997)'))}
-# {'bozo': True, 'entries': [], 'feed': {}, 'headers': {}, 'bozo_exception': URLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:997)'))}
-# {'bozo': True, 'entries': [], 'feed': {}, 'headers': {}, 'bozo_exception': URLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:997)'))}
-
-from importlib.metadata import entry_points
+# - Nothing right now!
 import feedparser
-from pprint import pprint
-from jinja2 import Environment, FileSystemLoader
-from flask import Flask, send_from_directory
 import os
+
+from flask import Flask, send_from_directory, redirect, url_for
+from importlib.metadata import entry_points
+from jinja2 import Environment, FileSystemLoader
+from pprint import pprint
+from threading import Thread
 
 app = Flask(__name__, static_folder='./html')
 
+
+# Update the settings here in order to add more feeds
 settings = {
     'kubernetes-blog': 'https://kubernetes.io/feed.xml',
     'azure-blog': 'https://azurecomcdn.azureedge.net/en-us/blog/feed/',
@@ -34,27 +31,43 @@ slug_tpl = env.get_template('slug.html.j2')
 with open('html/index.html', 'w') as index_fh:
     index_fh.write(index_tpl.render(slugs=settings.keys()))
 
-for slug, feed in settings.items():
-    content = list()
-    newsfeed = feedparser.parse(feed)
-    entries = newsfeed.entries
-    for article in entries:
-        content.append({
-            'title': article.title,
-            'published': article.published,
-            'link': article.link
-        })
-    settings[slug] = content
-    print(newsfeed)
-    with open(f'html/{slug}.html', 'w') as slug_fh:
-        slug_fh.write(slug_tpl.render(slugs=settings.keys(), rss_feed=content))
+def worker_func():
+    """
+        Start a thread to fetch and render rss feeds
+    """
+    thr = Thread(target=fetch_and_render, args=[settings])
+    thr.start()
+    return thr
+
+def fetch_and_render(settings:dict):
+    print("Updating feed...")
+    feeds = settings.copy()
+    for slug, feed in settings.items():
+        content = list()
+        newsfeed = feedparser.parse(feed)
+        entries = newsfeed.entries
+        for article in entries:
+            content.append({
+                'title': article.title,
+                'published': article.published,
+                'link': article.link
+            })
+        feeds[slug] = content
+        with open(f'html/{slug}.html', 'w') as slug_fh:
+            slug_fh.write(slug_tpl.render(slugs=feeds.keys(), rss_feed=content))
 
 @app.route('/', methods=['GET'])
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/<feed>', methods=['GET'])
-def feed(feed):
+def load_feed(feed):
     return send_from_directory(app.static_folder, feed)
 
+@app.route('/refresh', methods=['POST'])
+def refresh_content():
+    worker_func()
+    return redirect(url_for('.index')) 
+
+worker_func()
 app.run("0.0.0.0")
